0ea862d43911a6dc1fcffe0aad6bd1cb8e0ce744
ibsen4 <115497034+ibsen4@users.noreply.github.com>
2022-12-12 21:42:23 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,12 +1,13 @@
 #include "lib.h"
-int massimo( float a[],int dim) {
+float massimo(float a[],int dim) {
     float max;
-    max = a[0];
-    for (int i = 0; i < dim; i++) {
-        if (a[i] > max) {
-            max = a[i];
+    if (dim>0) {
+        max = a[0];
+        for (int i = 0; i < dim; i++) {
+            if (a[i] > max) {
+                max = a[i];
+            }
         }
     }
-
     return max;
 }

ef835a740bd28c3201e4a05fe2cb33a00f764f31
ibsen4 <115497034+ibsen4@users.noreply.github.com>
2022-12-12 21:20:56 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    int N;
+    cin >> N;
+    float a[N];
+    for (int i = 0; i < N; i++) {
+        cin >> a[i];
+    }
+    cout << massimo(a,N);
+    return 0;
 }
